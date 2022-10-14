3ebc9ac4eea0785db920cbe034c8fe4ec02d7d0c
Md. Abdulla Al Mamun <74006602+mdparvex@users.noreply.github.com>
2022-10-15 01:07:24 +0600

    Add files via upload

---
+++ deepfakeweb/app.py
@@ -0,0 +1,115 @@
+import os
+import uuid
+import flask
+import urllib
+from PIL import Image
+from tensorflow.keras.models import load_model
+from flask import Flask , render_template  , request , send_file
+from tensorflow.keras.preprocessing.image import load_img , img_to_array
+
+app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model = load_model(os.path.join(BASE_DIR , 'model.h5'))
+
+
+ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+
+
+
+
+def predict(filename , model):
+    img = load_img(filename , target_size = (224 , 224))
+    img = img_to_array(img)
+    img = img.reshape(1 , 224 ,224 ,3)
+
+    img = img.astype('float32')
+    img = img/255.0
+    result = model.predict(img)
+
+    if result[0][0]>=0.5:
+        prediction = "Real"
+        confidence = (result[0][0]*100).round(2)
+    elif result[0][0]<0.5:
+        prediction = "Fake"
+        confidence = 100- (result[0][0]*100).round(2)
+    else:
+        prediction="unknown"
+        confidence = "unknown"
+
+    return prediction, confidence, result
+
+
+
+
+@app.route('/')
+def home():
+        return render_template("index.html")
+
+@app.route('/success' , methods = ['GET' , 'POST'])
+def success():
+    error = ''
+    target_img = os.path.join(os.getcwd() , 'static/images')
+    if request.method == 'POST':
+        if(request.form):
+            link = request.form.get('link')
+            try :
+                resource = urllib.request.urlopen(link)
+                unique_filename = str(uuid.uuid4())
+                filename = unique_filename+".jpg"
+                img_path = os.path.join(target_img , filename)
+                output = open(img_path , "wb")
+                output.write(resource.read())
+                output.close()
+                img = filename
+
+                prediction, confidence, result = predict(img_path , model)
+
+                predictions = {
+                      "prediction": prediction,
+                      "confidence": confidence,
+                      'result': result
+                }
+
+            except Exception as e : 
+                print(str(e))
+                error = 'This image from this site is not accesible or inappropriate input'
+
+            if(len(error) == 0):
+                return  render_template('success.html' , img  = img , predictions = predictions)
+            else:
+                return render_template('index.html' , error = error) 
+
+            
+        elif (request.files):
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(target_img , file.filename))
+                img_path = os.path.join(target_img , file.filename)
+                img = file.filename
+
+                prediction, confidence, result = predict(img_path , model)
+
+                predictions = {
+                      "prediction": prediction,
+                      "confidence": confidence,
+                      "result": result
+                }
+
+            else:
+                error = "Please upload images of jpg , jpeg and png extension only"
+
+            if(len(error) == 0):
+                return  render_template('success.html' , img  = img , predictions = predictions)
+            else:
+                return render_template('index.html' , error = error)
+
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
